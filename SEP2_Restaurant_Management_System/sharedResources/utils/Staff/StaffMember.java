995192bef6c28e86e58ec352833f9ecbf3eba058
samosusa <samosusa123@gmail.com>
2024-04-19 10:59:21 +0200

    Whole model

--- SEP2_Restaurant_Management_System/sharedResources/utils/Staff/StaffMember.java
+++ SEP2_Restaurant_Management_System/sharedResources/utils/Staff/StaffMember.java
@@ -1,34 +1,40 @@
 package sharedResources.utils.Staff;
 
-
+import client.model.Reservation.ReservationList;
 import client.model.table.TableList;
+import sharedResources.utils.Customer.Customer;
+import sharedResources.utils.Reservation.Reservation;
+import sharedResources.utils.table.Table;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
-public class StaffMember extends Staff {
-    private TableList tableList;
 
-    public StaffMember(String name, int id) {
-        super(name, id);
+
+
+public class StaffMember {
+    private String name;
+
+    public StaffMember(String name) {
         this.name = name;
-        this.tableList = new TableList();
     }
 
-    public void addTable(int tableNumber, int capacity) {
-        tableList.createTable(tableNumber, capacity);
+    public String getName() {
+        return name;
     }
 
-    public void removeTable(int tableNumber) {
-        tableList.deleteTable(tableNumber);
+    public void makeReservation(ReservationList reservationList, Table table, LocalDateTime reservationTime) {
+        Reservation reservation = new Reservation(reservationTime, table);
+        reservation.reserveTable(reservationList, table);
     }
 
-
-    public void occupyTable(int tableNumber) {
-        tableList.updateTable(tableNumber, true);
+    public void clearReservation(ReservationList reservationList, Table table) {
+        Reservation reservation = reservationList.findReservationByTable(table.getTableNumber());
+        if (reservation != null) {
+            reservation.clearReservation(reservationList, table);
+        } else {
+            System.out.println("No reservation found for the specified table.");
+        }
     }
-
-
-    public void vacateTable(int tableNumber) {
-        tableList.updateTable(tableNumber, false);
-    }
-    }
+}
