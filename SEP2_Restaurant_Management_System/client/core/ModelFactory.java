71d9713348d3eaf2ea44258bd4926c4eaea4a967
samosusa <samosusa123@gmail.com>
2024-05-01 09:21:55 +0200

    .

--- SEP2_Restaurant_Management_System/client/core/ModelFactory.java
+++ SEP2_Restaurant_Management_System/client/core/ModelFactory.java
@@ -1,54 +1,84 @@
 package client.core;
 
+import client.model.Reservation.ReservationListModelManager;
 import client.model.Reservation.ReservationModel;
-import client.model.Reservation.ReservationModelManager;
+import client.model.kitchen.KitchenModel;
+import client.model.kitchen.KitchenModelManager;
 import client.model.login.LoginModel;
 import client.model.login.LoginModelManager;
+import client.model.staff.StaffModel;
+import client.model.staff.StaffModelManager;
+import client.model.table.TableListModelManager;
 import client.model.table.TableModel;
-import client.model.table.TableModelManager;
 
 import java.rmi.RemoteException;
 
 public class ModelFactory {
 
     private final ClientFactory client;
-    private LoginModel  loginModel;
-    private TableModel tablemodel;
-    private ReservationModel reservationmodel;
+    private LoginModel loginModel;
+    private TableModel tableManagementModel;
+    private ReservationModel reservationModel;
+    private StaffModel staffModel;
+    private KitchenModel kitchenModel;
 
-    public ModelFactory (ClientFactory client) {
+    public ModelFactory(ClientFactory client) {
         this.client = client;
     }
 
-    public LoginModel getLoginModel(){
-       if (loginModel == null){
-           try {
-               loginModel = new LoginModelManager(client.getLoginClient());
-           } catch (RemoteException e) {
-               throw new RuntimeException(e);
-           }
-       }
-       return loginModel;
+    public LoginModel getLoginModel() {
+        if (loginModel == null) {
+            try {
+                loginModel = new LoginModelManager(client.getLoginClient());
+            } catch (RemoteException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return loginModel;
     }
 
     public TableModel getTableModel() {
-        if (tablemodel == null){
+        if (tableManagementModel == null) {
             try {
-                tablemodel = new TableModelManager(client.getTableClient());
+                tableManagementModel = new TableListModelManager(client.getTableClient());
             } catch (RemoteException e) {
                 throw new RuntimeException(e);
             }
         }
-        return tablemodel;
+        return tableManagementModel;
     }
+
     public ReservationModel getReservationModel() {
-        if (reservationmodel == null){
+        if (reservationModel == null) {
             try {
-                reservationmodel = new ReservationModelManager(client.getTableClient());
+                reservationModel = new ReservationListModelManager(client.getReservationClient());
             } catch (RemoteException e) {
                 throw new RuntimeException(e);
             }
         }
-        return reservationmodel;
+        return reservationModel;
+    }
+
+    public StaffModel getStaffModel() {
+        if (staffModel == null) {
+            try {
+                staffModel = new StaffModelManager(client.getStaffClient());
+            } catch (RemoteException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return staffModel;
+
+    }
+
+    public KitchenModel getKitchenModel() {
+        if (kitchenModel == null) {
+            try {
+                kitchenModel = new KitchenModelManager(client.getKitchenClient());
+            } catch (RemoteException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return kitchenModel;
     }
 }
