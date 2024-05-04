91e715df2abcc468b60f28add9d78602aee75c38
samosusa <samosusa123@gmail.com>
2024-05-04 13:41:57 +0200

    .

--- SEP2_Restaurant_Management_System/server/database/DatabaseConnection.java
+++ SEP2_Restaurant_Management_System/server/database/DatabaseConnection.java
@@ -8,6 +8,6 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection
-                ("jdbc:postgresql://localhost:5432/postgres?currentSchema=rms_sep2", "postgres", "P@ssw0rd");
+                ("jdbc:postgresql://localhost:5432/postgres?currentSchema=rms_sep2", "postgres", "captos123");
     }
 }
